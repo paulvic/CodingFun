6497586f38578c341642e3090771d370ebd8914e
paulvic <paul_vickers@outlook.com>
2017-01-26 19:21:20 +0000

    Singly linked list implementation

---
+++ DataStructures/DataStructures/LinkedList/Node.cs
@@ -0,0 +1,28 @@
+﻿namespace DataStructures.LinkedList
+{
+    /// <summary>
+    /// A node in a linked list
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public class Node<T>
+    {
+        /// <summary>
+        /// The node value
+        /// </summary>
+        public T Value { get; set; }
+
+        /// <summary>
+        /// The next node in the list, null if last node (tail)
+        /// </summary>
+        public Node<T> Next { get; set; }
+
+        /// <summary>
+        /// Constructs a new node with the specified value
+        /// </summary>
+        /// <param name="value"></param>
+        public Node(T value)
+        {
+            this.Value = value;
+        }
+    }
+}
