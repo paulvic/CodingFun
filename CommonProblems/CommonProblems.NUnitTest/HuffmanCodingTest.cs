f8fb4c020ac38894d54049a1e8f1d7f3e8848271
paulvic <paul_vickers@outlook.com>
2017-03-19 21:45:34 +0000

    Huffman nodes populated

---
+++ CommonProblems/CommonProblems.NUnitTest/HuffmanCodingTest.cs
@@ -0,0 +1,88 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CommonProblems.NUnit.Tests
+{
+    [TestFixture]
+    public class HuffmanCodingTest
+    {
+        private const string TestString2 = "this is an example of a huffman tree";
+        private const string TestString1 = "This is an interesting test string about a quick brown fox";
+//T
+//h
+//iiiii
+//ssss
+
+//aaa
+//nnnnn
+//tttttt
+//ee
+//rrr
+//gg
+//bb
+//ooo
+//uu
+//q
+//c
+//k
+//w
+//f
+//x
+
+        
+        [Test]
+        public void CanInstantiateClass()
+        {
+            HuffmanTree huffman = new HuffmanTree(TestString1);
+        }
+
+        [Test]
+        public void IsUniqueCharacterCountCorrect()
+        {
+            HuffmanTree huffman = new HuffmanTree(TestString1);
+            Dictionary<char, int> characters = huffman.Characters;
+            Assert.AreEqual(20, characters.Count);
+        }
+
+        [Test]
+        public void IsCharacterFrequencyCorrect()
+        {
+            HuffmanTree huffman = new HuffmanTree(TestString1);
+            Dictionary<char, int> characters = huffman.Characters;
+            Assert.AreEqual(1, characters['T']);
+            Assert.AreEqual(6, characters['i']);
+            Assert.AreEqual(1, characters['f']);
+            Assert.AreEqual(1, characters['x']);
+        }
+
+        [Test]
+        public void IsUniqueCharacterCountCorrectTestString2()
+        {
+            HuffmanTree huffman = new HuffmanTree(TestString2);
+            Dictionary<char, int> characters = huffman.Characters;
+            Assert.AreEqual(16, characters.Count);
+        }
+
+        [Test]
+        public void IsCharacterFrequencyCorrectTestString2()
+        {
+            HuffmanTree huffman = new HuffmanTree(TestString2);
+            Dictionary<char, int> characters = huffman.Characters;
+            Assert.AreEqual(2, characters['t']);
+            Assert.AreEqual(4, characters['e']);
+            Assert.AreEqual(7, characters[' ']);
+            Assert.AreEqual(1, characters['x']);
+        }
+        
+        [Test]
+        public void AreCharactersSortedByFrequencyThenCharacter()
+        {
+            HuffmanTree huffman = new HuffmanTree(TestString2);
+            SortedSet<HuffmanNode> nodes = huffman.Nodes;
+            Assert.AreEqual('l', nodes.Min.Character);
+            Assert.AreEqual(' ', nodes.Max.Character);
+        }
+    }
+}
---
+++ CommonProblems/CommonProblems/HuffmanCoding.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommonProblems
+{
+    public class HuffmanNode : IComparable<HuffmanNode>
+    {
+        public HuffmanNode Parent { get; set; }
+        public HuffmanNode Left { get; set; }
+        public HuffmanNode Right { get; set; }
+        public char Character { get; set; }
+        public int Frequency { get; set; }
+
+        public int CompareTo(HuffmanNode other)
+        {
+            int returnVal = -1;
+
+            if (Frequency == other.Frequency)
+            {
+                if (Character == other.Character)
+                {
+                    return 0;
+                }
+                else if (Character < other.Character)
+                {
+                    return -1;
+                }
+                else
+                {
+                    returnVal = 1;
+                }
+            }
+            else if (Frequency < other.Frequency)
+            {
+                returnVal = -1;
+            }
+            else
+            {
+                returnVal = 1;
+            }
+
+            return returnVal;
+        }
+    }
+
+    public class HuffmanTree
+    {
+        private Dictionary<char, int> characters;
+        private SortedSet<HuffmanNode> nodes;
+
+        public HuffmanTree(string input)
+        {
+            characters = new Dictionary<char, int>();
+
+            foreach (var character in input)
+            {
+                if (!characters.ContainsKey(character))
+                {
+                    characters.Add(character, 0);
+                }
+
+                characters[character]++;
+            }
+
+            nodes = new SortedSet<HuffmanNode>();
+            foreach (KeyValuePair<char, int> character in characters)
+            {
+                nodes.Add(new HuffmanNode() { Character = character.Key, Frequency = character.Value });
+            }
+
+            while (nodes.Count > 1)
+            {
+                var node1 = nodes.Min;
+                nodes.Remove(node1);
+                var node2 = nodes.Min;
+                nodes.Remove(node2);
+                var parent = new HuffmanNode() { Left = node1, Right = node2, Frequency = node1.Frequency + node2.Frequency };
+                node1.Parent = parent;
+                node2.Parent = parent;
+                nodes.Add(parent);
+            }
+        }
+
+        public Dictionary<char, int> Characters
+        {
+            get { return characters; }
+            private set { characters = value; }
+        }
+
+
+        public SortedSet<HuffmanNode> Nodes
+        {
+            get { return nodes; }
+            private set { nodes = value; }
+        }
+
+    }
+}
