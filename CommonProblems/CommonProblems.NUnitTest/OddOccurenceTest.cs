c9a99133ffaf902eb16b06e517e49086e7059bad
paulvic <paul_vickers@outlook.com>
2017-03-22 13:40:58 +0000

    OddOccurencesInArray

---
+++ CommonProblems/CommonProblems.NUnitTest/OddOccurenceTest.cs
@@ -0,0 +1,126 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace CommonProblems.NUnit.Tests
+{
+    [TestFixture]
+    class OddOccurenceTest
+    {
+        #region Generic Implementation
+        [Test]
+        public void ArrayContainingNoElementsThrowsException()
+        {
+            OddOccurence<char> odd = new OddOccurence<char>();
+            ArgumentException ex = Assert.Throws<ArgumentException>(
+                delegate { odd.GetOddOccurence(new char[]{}); });
+        }
+
+        [Test]
+        public void ArrayContainingOneElementReturnsThatElement()
+        {
+            OddOccurence<char> odd = new OddOccurence<char>();
+            Assert.AreEqual('a', odd.GetOddOccurence(new char[]{ 'a' }));
+        }
+
+        [Test]
+        public void ArrayContainingTwoElementsThrowsException()
+        {
+            OddOccurence<char> odd = new OddOccurence<char>();
+            ArgumentException ex = Assert.Throws<ArgumentException>(
+                delegate { odd.GetOddOccurence(new char[] { 'a', 'b' }); });
+        }
+
+        [Test]
+        public void ArrayContainingOneOddElementOccurenceReturnsThatElement()
+        {
+            OddOccurence<char> odd = new OddOccurence<char>();
+            Assert.AreEqual('c', odd.GetOddOccurence(new char[] { 'a', 'a', 'b', 'b', 'c', 'c', 'c' }));
+        }
+
+        [Test]
+        public void ArrayContainingTwoOddElementOccurencesThrowsException()
+        {
+            OddOccurence<char> odd = new OddOccurence<char>();
+            ArgumentException ex = Assert.Throws<ArgumentException>(
+                delegate { odd.GetOddOccurence(new char[] { 'a', 'a', 'a', 'b', 'b', 'c', 'c', 'c' }); });
+        }
+        
+        [Test]
+        public void ArrayContainingNoIntsThrowsException()
+        {
+            OddOccurence<int> odd = new OddOccurence<int>();
+            ArgumentException ex = Assert.Throws<ArgumentException>(
+                delegate { odd.GetOddOccurence(new int[] { }); });
+        }
+
+        [Test]
+        public void ArrayContainingOneIntReturnsThatElement()
+        {
+            OddOccurence<int> odd = new OddOccurence<int>();
+            Assert.AreEqual(1, odd.GetOddOccurence(new int[] { 1 }));
+        }
+
+        [Test]
+        public void ArrayContainingTwoIntsThrowsException()
+        {
+            OddOccurence<int> odd = new OddOccurence<int>();
+            ArgumentException ex = Assert.Throws<ArgumentException>(
+                delegate { odd.GetOddOccurence(new int[] { 1, 2 }); });
+        }
+
+        [Test]
+        public void ArrayContainingOneOddIntOccurenceReturnsThatElement()
+        {
+            OddOccurence<int> odd = new OddOccurence<int>();
+            Assert.AreEqual(3, odd.GetOddOccurence(new int[] { 1, 1, 2, 2, 3, 3, 3 }));
+        }
+
+        [Test]
+        public void ArrayContainingTwoOddIntsThrowsException()
+        {
+            OddOccurence<int> odd = new OddOccurence<int>();
+            ArgumentException ex = Assert.Throws<ArgumentException>(
+                delegate { odd.GetOddOccurence(new int[] { 1, 1, 1, 2, 2, 2, 3, 3 }); });
+        }
+        #endregion
+
+        #region int implementation
+        [Test]
+        public void IntArrayContainingNoIntsThrowsException()
+        {
+            OddOccurence<int> odd = new OddOccurence<int>();
+            ArgumentException ex = Assert.Throws<ArgumentException>(
+                delegate { odd.GetIntOddOccurence(new int[] { }); });
+        }
+
+        [Test]
+        public void IntArrayContainingOneIntReturnsThatElement()
+        {
+            OddOccurence<int> odd = new OddOccurence<int>();
+            Assert.AreEqual(1, odd.GetIntOddOccurence(new int[] { 1 }));
+        }
+
+        [Test]
+        public void IntArrayContainingTwoIntsThrowsException()
+        {
+            OddOccurence<int> odd = new OddOccurence<int>();
+            ArgumentException ex = Assert.Throws<ArgumentException>(
+                delegate { odd.GetIntOddOccurence(new int[] { 1, 2 }); });
+        }
+
+        [Test]
+        public void IntArrayContainingOneOddIntOccurenceReturnsThatElement()
+        {
+            OddOccurence<int> odd = new OddOccurence<int>();
+            Assert.AreEqual(3, odd.GetIntOddOccurence(new int[] { 1, 1, 2, 2, 3, 3, 3 }));
+        }
+
+        [Test]
+        public void IntArrayContainingTwoOddIntOccurencesReturnsWrongValue()
+        {
+            OddOccurence<int> odd = new OddOccurence<int>();
+            Assert.AreEqual(3, odd.GetIntOddOccurence(new int[] { 1, 1, 1, 2, 2, 2, 3, 3 }));
+        }
+        #endregion
+    }
+}
---
+++ CommonProblems/CommonProblems/OddOccurence.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CommonProblems
+{
+    public class OddOccurence<T>
+    {
+        public T GetOddOccurence(T[] array)
+        {
+            if (array.Length == 0)
+            {
+                throw new ArgumentException("Array must not be empty");
+            }
+            else if (array.Length == 2)
+            {
+                throw new ArgumentException("Array cannot contain just two elements");
+            }
+ 
+            HashSet<T> numberTracker = new HashSet<T>();
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (numberTracker.Contains(array[i]))
+                {
+                    numberTracker.Remove(array[i]);
+                }
+                else
+                {
+                    numberTracker.Add(array[i]);
+                }
+            }
+
+            if (numberTracker.Count > 1)
+            {
+                throw new ArgumentException("Array more than one element that occurs an odd number of times");
+            }
+
+            return numberTracker.First();
+        }
+
+        public int GetIntOddOccurence(int[] array)
+        {
+            if (array.Length == 0)
+            {
+                throw new ArgumentException("Array must not be empty");
+            }
+            else if (array.Length == 2)
+            {
+                throw new ArgumentException("Array cannot contain just two elements");
+            }
+
+            int oddNumber = 0;
+            for (int i = 0; i < array.Length; i++)
+            {
+                oddNumber = oddNumber ^ array[i];
+            }
+
+            return oddNumber;
+        }
+    }
+}
